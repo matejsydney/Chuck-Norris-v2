3ceef1c72c9825a4ab44737e2ef90fc7b58620c1
Matej <matejStolfa@gmail.com>
2019-10-07 16:45:02 +1100

    Removing suggestion as they don't want to work

--- app.py
+++ app.py
@@ -67,27 +67,12 @@ def makeWebhookResultForGetJokev2():
     speechText = "<speak>" + joke + '<break time="2s"/>' + " Would you like another joke?" + "</speak>"
     displayText = joke + " Would you like another joke???"
     return {
-            {
                 "fulfillmentText": speechText,
                 "fulfillmentMessages": [{"simpleResponses": {"simpleResponses": [   {
                     "textToSpeech": speechText,
                     "displayText": displayText
-                }]}}],
-            },
-            {
-            "suggestions": {
-                "suggestions": [
-                    {
-                        "title": "Another joke"
-                    },
-                    {
-                        "title": "Cancel"
-                    }
-                ]
-            }
-            }
+                }]}}]
         }
-
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
